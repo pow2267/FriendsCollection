79cc5f027a3841b15f40d5ce7340371af6f894c1
pow2267 <pow2267@gmail.com>
2021-08-14 14:09:31 +0900

    Add flow layout

---
+++ FriendsCollection/FriendCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendCollectionViewCell.swift
+//  FriendsCollection
+//
+//  Created by kwon on 2021/08/13.
+//
+
+import UIKit
+
+class FriendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var nameAgeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
+}
--- FriendsCollection/ViewController.swift
+++ FriendsCollection/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let cellIdentifier: String = "cell"
+    var friends: [Friend] = []
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.friends.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: FriendCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellIdentifier, for: indexPath) as? FriendCollectionViewCell else {
+            preconditionFailure("오류가 발생했습니다.")
+        }
+        
+        let friend: Friend = self.friends[indexPath.item]
+        
+        cell.nameAgeLabel.text = friend.nameAndAge
+        cell.addressLabel.text = friend.fullAddress
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let flowLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets.zero
+        flowLayout.minimumInteritemSpacing = 50
+        flowLayout.minimumLineSpacing = 20
+        
+        let halfWidth: CGFloat = UIScreen.main.bounds.width / 2.0
+        
+        flowLayout.estimatedItemSize = CGSize(width: halfWidth - 30, height: 90)
+        
+        self.collectionView.collectionViewLayout = flowLayout
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset:NSDataAsset = NSDataAsset(name: "friends") else {
+            return
+        }
+        
+        do {
+            self.friends = try jsonDecoder.decode([Friend].self, from: dataAsset.data)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        self.collectionView.reloadData()
     }
-
-
 }
 
